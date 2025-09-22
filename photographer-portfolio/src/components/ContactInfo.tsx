4059d43fe88d23a66d5f4802c3179263b8300eb4
Alvaro Canovas <hello@acanovas.info>
2025-09-22 20:17:06 +0200

    Make email and phone clickable links

--- photographer-portfolio/src/components/ContactInfo.tsx
+++ photographer-portfolio/src/components/ContactInfo.tsx
@@ -7,8 +7,12 @@ interface ContactInfoProps {
 export default function ContactInfo({ className }: ContactInfoProps) {
   return (
     <div className={className ? className : ''}>
-      <p>info@lluiscanovas.com</p>
-      <p>ES +34 682 665 624</p>
+      <p>
+        <a href="mailto:info@lluiscanovas.com">info@lluiscanovas.com</a>
+      </p>
+      <p>
+        <a href="tel:+34683665624">ES +34 683 665 624</a>
+      </p>
     </div>
   );
 }
